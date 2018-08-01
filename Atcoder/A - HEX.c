2910d7e1e10c8a2404c707ebc2a3c7f5bc0bd6f8
aditya995 <39437959+aditya995@users.noreply.github.com>
2018-08-02 04:06:41 +0600

    ok

---
+++ Atcoder/A - HEX.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char X,Y;
+    scanf("%c %c",&X,&Y);
+    if(X<Y){
+        printf("<\n");
+    }else if(X>Y){
+        printf(">\n");
+    }else{
+        printf("=\n");
+    }
+    return 0;
+}
